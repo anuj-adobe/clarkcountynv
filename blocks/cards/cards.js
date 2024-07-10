65751054fb4d227003c9701198f0b5b05217175f
Anuj Mahajan <anujmaha@adobe.com>
2024-07-10 20:52:57 +0530

    Add accepted blocks from AEM Assistant

--- blocks/cards/cards.js
+++ blocks/cards/cards.js
@@ -1,18 +1,29 @@
-import { createOptimizedPicture } from '../../scripts/aem.js';
 
 export default function decorate(block) {
-  /* change to ul, li */
-  const ul = document.createElement('ul');
-  [...block.children].forEach((row) => {
-    const li = document.createElement('li');
-    while (row.firstElementChild) li.append(row.firstElementChild);
-    [...li.children].forEach((div) => {
-      if (div.children.length === 1 && div.querySelector('picture')) div.className = 'cards-card-image';
-      else div.className = 'cards-card-body';
-    });
-    ul.append(li);
+  const cards = block.querySelectorAll('.cards > div');
+
+  cards.forEach((card) => {
+    card.classList.add('card');
+    const picture = card.querySelector('picture');
+    const category = card.querySelectorAll('p')[0];
+    const description = card.querySelectorAll('p')[1];
+    const linkContainer = card.querySelector('p:last-child');
+    const link = linkContainer.querySelector('a');
+
+    const imgContainer = document.createElement('div');
+    imgContainer.classList.add('card-image');
+    imgContainer.appendChild(picture);
+
+    const textContainer = document.createElement('div');
+    textContainer.classList.add('card-text');
+    textContainer.appendChild(category);
+    textContainer.appendChild(description);
+    textContainer.appendChild(linkContainer);
+
+    link.classList.add('card-link');
+
+    card.innerHTML = '';
+    card.appendChild(imgContainer);
+    card.appendChild(textContainer);
   });
-  ul.querySelectorAll('img').forEach((img) => img.closest('picture').replaceWith(createOptimizedPicture(img.src, img.alt, false, [{ width: '750' }])));
-  block.textContent = '';
-  block.append(ul);
 }
