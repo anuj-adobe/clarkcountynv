fd726f517afc21e710756911db431fa646325f69
Anuj Mahajan <anujmaha@adobe.com>
2024-07-16 20:41:27 +0530

    Adding blocks from block collection

--- blocks/cards/cards.js
+++ blocks/cards/cards.js
@@ -1,29 +1,18 @@
+import { createOptimizedPicture } from '../../scripts/aem.js';
 
 export default function decorate(block) {
-  // Setting up the block structure
-  block.classList.add('cards');
-  const cardItems = Array.from(block.children);
-
-  cardItems.forEach((item) => {
-    item.classList.add('card');
-    const [imageWrapper, textWrapper] = item.children;
-    imageWrapper.classList.add('card-image');
-    textWrapper.classList.add('card-text');
-
-    const [title, description, linkWrapper] = textWrapper.children;
-    title.classList.add('card-title');
-    description.classList.add('card-description');
-    const link = linkWrapper.querySelector('a');
-    link.classList.add('card-link');
-
-    // Wrapping the link text in a span for styling
-    const linkText = link.innerText;
-    link.innerHTML = `<span>${linkText}</span>`;
-
-    // Adding icon to the link
-    const icon = document.createElement('span');
-    icon.classList.add('card-link-icon');
-    icon.innerHTML = '➔'; // Arrow icon
-    link.appendChild(icon);
+  /* change to ul, li */
+  const ul = document.createElement('ul');
+  [...block.children].forEach((row) => {
+    const li = document.createElement('li');
+    while (row.firstElementChild) li.append(row.firstElementChild);
+    [...li.children].forEach((div) => {
+      if (div.children.length === 1 && div.querySelector('picture')) div.className = 'cards-card-image';
+      else div.className = 'cards-card-body';
+    });
+    ul.append(li);
   });
+  ul.querySelectorAll('img').forEach((img) => img.closest('picture').replaceWith(createOptimizedPicture(img.src, img.alt, false, [{ width: '750' }])));
+  block.textContent = '';
+  block.append(ul);
 }
